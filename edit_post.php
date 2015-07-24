77af153ee87b765f067f59c5810cfb0f3383b1ad
Anna Poterek <ahpoterek@gmail.com>
2015-07-23 20:24:39 -0500

    Beginning to access the information

--- edit_post.php
+++ edit_post.php
@@ -1,4 +1,14 @@
 <form action= "edit_post.php" method= "POST">
-<input type= "text" name= "title" value= "Title"><br>
-<textarea name= "post" rows= "10" cols= "40"></textarea><br>
+<input type= "text" name= "title" value= "Title your post"><br>
+<input type= "text" name= "author" value= "Who are you?"><br>
+<textarea name= "post" rows= "10" cols= "40">Write something!</textarea><br>
+
 <input type= "submit" name= "submit" value= "Submit"><br>
+
+<?php
+if (isset($_POST["submit"])){
+	$title = $_POST['title'];
+	$author= $_POST['author'];
+	$content= $_POST['post'];
+}
+?>
