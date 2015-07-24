ffa201f6ecf3955b1adf6f8571160c21287f5084
Anna Poterek <ahpoterek@gmail.com>
2015-07-23 20:12:34 -0500

    Added the basic post form

---
+++ edit_post.php
@@ -0,0 +1,4 @@
+<form action= "edit_post.php" method= "POST">
+<input type= "text" name= "title" value= "Title"><br>
+<textarea name= "post" rows= "10" cols= "40"></textarea><br>
+<input type= "submit" name= "submit" value= "Submit"><br>
