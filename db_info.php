e5867a02ebb73c401448ebaafa9487481c6d4ff8
Anna Poterek <ahpoterek@gmail.com>
2015-08-04 18:12:55 -0500

    Creating posts

--- db_info.php
+++ db_info.php
@@ -9,6 +9,40 @@ if ($db-> connect_errno){
 	exit();
 }
 
+//CREATE POST
+if (isset($_POST["create"])){
+	//create variables
+	$cr_title = $_POST['title'];
+	$cr_author= $_POST['author'];
+	$cr_content= $_POST['post'];
+	//input validation
+	if ($cr_title== "" || $cr_title== "Title your post" || $cr_author== "" || $cr_author == "Who are you?"|| $cr_content == "" || $cr_content == "Write something!"){
+		return "Please enter valid information";
+	}else{
+		//prepare statement
+		$create= $db->prepare('INSERT INTO posts(title, author, date, modified, contents) VALUES (?,?,?,?)');
+		//define additional variables
+		$cr_date= date("l.m.d.Y");
+		$cr_modified= $date;
+		//bind parameters 
+		$create->bind_param('sssss', $cr_title, $cr_author, $cr_date, $cr_modified, $cr_content);
+		//execute
+		$create->execute();
+	}	
+}
+
+//DELETE POST
+if (isset($_POST["delete"])){
+	//prepare
+	$delete= $db->prepare('DELETE FROM posts WHERE id=?');
+	//create variables
+	$d_id= $_POST['id'];
+	//bind param
+	$delete->bind_param('i', $d_id);
+	//execute
+	$delete->execute();
+
+}
 
 
 ?>
